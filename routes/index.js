5c49f2c8f19486c208fccb893ed1bb84e1236745
bearcatat <807651533@qq.com>
2017-03-17 23:47:05 +0800

    add index

--- routes/index.js
+++ routes/index.js
@@ -6,12 +6,20 @@ var superagent = require('superagent');
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
- router.get('/index/ranking-3day.json',function(req,res,next){
-   superagent.get('http://www.bilibili.com/index/ranking-3day.json').end(function(err,sres){
-     if(err){
-       console.log(err);
-     }
-     res.send(JSON.parse(sres.text));
-   })
+router.get('/index/ranking-3day.json',function(req,res,next){
+ superagent.get('http://www.bilibili.com/index/ranking-3day.json').end(function(err,sres){
+   if(err){
+     console.log(err);
+   }
+   res.send(JSON.parse(sres.text));
  })
+})
+router.get('/typedynamic/index',function(req,res,next){
+  superagent.get('http://api.bilibili.com'+req.url).end(function(err,sres){
+    if(err){
+      console.log(err);
+    }
+    res.send(JSON.parse(sres.text));
+  })
+})
 module.exports = router;
